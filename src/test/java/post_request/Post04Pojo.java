61904175cfdbfc3984104339f1595c5ddf18f569
Ebru Aydin <aydinnebrr@gmail.com>
2022-07-18 22:18:17 +0200

    07.18.2022

--- src/test/java/post_request/Post04Pojo.java
+++ src/test/java/post_request/Post04Pojo.java
@@ -1,4 +1,48 @@
 package post_request;
 
-public class Post04Pojo {
+import base_urls.HerOkuAppBaseUrl;
+import org.junit.Test;
+
+public class Post04Pojo extends HerOkuAppBaseUrl {
+    /*
+         Given
+            https://restful-booker.herokuapp.com/booking
+            {
+                "firstname": "Ali",
+                "lastname": "Can",
+                "totalprice": 999,
+                "depositpaid": true,
+                "bookingdates": {
+                    "checkin": "2021-09-21",
+                    "checkout": "2021-12-21"
+                 }
+                 "additionalneeds": "Breakfast with white tea, Dragon juice"
+             }
+        When
+ 		    I send POST Request to the URL
+ 	    Then
+ 		    Status code is 200
+ 		And
+ 		    Response body is like {
+ 		                            "bookingid": 16,
+ 		                            "booking" :{
+                                        "firstname": "Ali",
+                                        "lastname": "Can",
+                                        "totalprice": 999,
+                                        "depositpaid": true,
+                                        "bookingdates": {
+                                            "checkin": "2021-09-21",
+                                            "checkout": "2021-12-21"
+                                        }
+                                        "additionalneeds": "Breakfast with white tea, Dragon juice"
+                                     }
+                                  }
+     */
+
+
+    @Test
+    public void post04(){
+        //1. set the url
+        spec.pathParams("first", "booking");
+    }
 }
